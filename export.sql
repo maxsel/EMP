51618c2508c46640da83d1cede20626bad526a93
Maksim Sialiuk <Maksim_Sialiuk@epam.com>
2016-09-08 15:11:30 +0300

    'tree'

---
+++ export.sql
@@ -0,0 +1,21 @@
+REM INSERTING into EXPORT_TABLE
+SET DEFINE OFF;
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (1,1,'A','A');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (2,2,'AB',' B');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (6,3,'ABF','  F');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (14,4,'ABFN','   N');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (15,4,'ABFO','   O');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (7,3,'ABG','  G');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (16,4,'ABGP','   P');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (8,3,'ABH','  H');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (3,2,'AC',' C');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (9,3,'ACI','  I');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (17,4,'ACIQ','   Q');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (18,4,'ACIR','   R');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (10,3,'ACJ','  J');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (4,2,'AD',' D');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (11,3,'ADK','  K');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (19,4,'ADKS','   S');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (12,3,'ADL','  L');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (5,2,'AE',' E');
+Insert into EXPORT_TABLE (EMP_ID,LEV,PATHSTR,NAME) values (13,3,'AEM','  M');

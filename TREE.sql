a165b7ee4ba5f20985958f92af92a154e21354b8
Maksim Sialiuk <Maksim_Sialiuk@epam.com>
2016-09-09 16:30:22 +0300

    formatted

--- TREE.sql
+++ TREE.sql
@@ -3,20 +3,20 @@ FROM EMP
 JOIN (
     SELECT WITH_PATH.EMP_ID, WITH_PATH.PATH
     FROM (
-        WITH TREE(EMP_ID, PATH)
-        AS
-        (
-                SELECT EMP.EMP_ID, ','||EMP.EMP_ID||','
-                FROM EMP
-                WHERE EMP.MAN_ID IS NULL
-            UNION ALL
-                SELECT EMP.EMP_ID, TREE.PATH||EMP.EMP_ID||','
-                FROM EMP
-                JOIN TREE ON TREE.EMP_ID = EMP.MAN_ID
-        )
-        SELECT TREE.EMP_ID, TREE.PATH
-        FROM TREE
-    ) WITH_PATH
+            WITH TREE(EMP_ID, PATH)
+            AS
+            (
+                    SELECT EMP.EMP_ID, ','||EMP.EMP_ID||','
+                    FROM EMP
+                    WHERE EMP.MAN_ID IS NULL
+                UNION ALL
+                    SELECT EMP.EMP_ID, TREE.PATH||EMP.EMP_ID||','
+                    FROM EMP
+                    JOIN TREE ON TREE.EMP_ID = EMP.MAN_ID
+            )
+            SELECT TREE.EMP_ID, TREE.PATH
+            FROM TREE
+        ) WITH_PATH
 ) EMP_PATH
 ON EMP_PATH.PATH LIKE '%,'||EMP.EMP_ID||',%'
 GROUP BY EMP.EMP_ID
